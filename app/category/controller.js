24e5a361f04edea586909b1ea58446d3c259b0ae
zdn <library.devz@gmail.com>
2024-12-29 00:58:36 +0700

    feat: crud category

--- app/category/controller.js
+++ app/category/controller.js
@@ -3,7 +3,10 @@ const Category = require("./model");
 module.exports = {
   index: async (req, res) => {
     try {
-      res.render("admin/category/view_category");
+      const category = await Category.find();
+      res.render("admin/category/view_category", {
+        category,
+      });
     } catch (error) {
       console.log(error);
     }
@@ -15,13 +18,45 @@ module.exports = {
       console.log(error);
     }
   },
-
   actionCreate: async (req, res) => {
     try {
       const { name } = req.body;
 
       let category = new Category({ name });
       await category.save();
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.findOne({ _id: id });
+
+      res.render("admin/category/edit", {
+        category,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name } = req.body;
+      await Category.findOneAndUpdate({ _id: id }, { name });
+
+      res.redirect("/category");
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      await Category.findOneAndRemove({ _id: id });
 
       res.redirect("/category");
     } catch (error) {
