66fd505590118f317200ba08c16eb4d63b9ad53e
zdn <library.devz@gmail.com>
2024-12-31 02:57:13 +0700

    feat: create voucher

---
+++ app/voucher/model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+let voucherSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Nama game harus diisi"],
+  },
+  status: {
+    type: String,
+    enum: ["Y", "N"],
+    default: "Y",
+  },
+  thumbnail: {
+    type: String,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  nominals: {
+    type: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Nominal",
+      },
+    ],
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+});
+
+module.exports = mongoose.model("Voucher", voucherSchema);
