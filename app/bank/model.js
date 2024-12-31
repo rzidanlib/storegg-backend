4287528e4e8e2f299e1d92f804bda4d65af35337
zdn <library.devz@gmail.com>
2025-01-01 03:56:52 +0700

    feat: crud bank

---
+++ app/bank/model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+let bankSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Nama pemilik akun bank harus diisi"],
+  },
+  bankName: {
+    type: String,
+    required: [true, "Nama bank harus diisi"],
+  },
+  noRekening: {
+    type: String,
+    required: [true, "Nomor rekening harus diisi"],
+  },
+});
+
+module.exports = mongoose.model("Bank", bankSchema);
