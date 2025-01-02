b6e29c78c67aa0355a77ba00ea98f1f9806436f9
zdn <library.devz@gmail.com>
2025-01-03 00:36:15 +0700

    feat: update status transaction

--- app/transaction/controller.js
+++ app/transaction/controller.js
@@ -20,4 +20,21 @@ module.exports = {
       res.redirect("/transaction");
     }
   },
+
+  actionStatus: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { status } = req.query;
+
+      await Transaction.findByIdAndUpdate({ _id: id }, { status });
+
+      req.flash("alertMessage", "Berhasil ubah status");
+      req.flash("alertStatus", "success");
+      res.redirect("/transaction");
+    } catch (error) {
+      req.flash("alertMessage", `${error.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/transaction");
+    }
+  },
 };
