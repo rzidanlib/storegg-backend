d92cf74b30fef85326584c4c554ecaf60d1a619d
zdn <library.devz@gmail.com>
2025-01-04 21:50:35 +0700

    feat: api detail voucher

--- app/player/controller.js
+++ app/player/controller.js
@@ -12,6 +12,27 @@ module.exports = {
     } catch (error) {
       res
         .status(500)
+        .json({ message: error.message || "Internal server error" });
+    }
+  },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const voucher = await Voucher.findOne({ _id: id })
+        .populate("category")
+        .populate("nominals")
+        .populate("user", "_id name phoneNumber");
+
+      if (!voucher) {
+        return res
+          .status(404)
+          .json({ message: "Voucher game tidak ditemukan" });
+      }
+
+      res.status(200).json({ data: voucher });
+    } catch (error) {
+      res
+        .status(500)
         .json({ message: error.message || "Terjadi kesalahan pada server" });
     }
   },
