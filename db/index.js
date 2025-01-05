1d0a024d5b813d1506ca5f895c6da96281166021
zdn <library.devz@gmail.com>
2025-01-05 20:05:27 +0700

    change mongoose version

--- db/index.js
+++ db/index.js
@@ -4,10 +4,7 @@ const { urlDb } = require("../config");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(urlDb, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  await mongoose.connect(urlDb);
   console.log("Database connected");
 }
 
