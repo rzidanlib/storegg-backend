3c1fec0735985ff3b4b25c542207f8fe2b727170
zdn <library.devz@gmail.com>
2025-01-05 17:36:47 +0700

    change db config to before

--- db/index.js
+++ db/index.js
@@ -15,8 +15,4 @@ async function main() {
 }
 
 const db = mongoose.connection;
-
-db.on("error", (err) => console.error("MongoDB connection error:", err));
-db.once("open", () => console.log("MongoDB connection successful"));
-
 module.exports = db;
