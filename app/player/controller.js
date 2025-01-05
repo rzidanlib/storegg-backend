1586c989deb8a079d65dd41edaa397497d8093b4
zdn <library.devz@gmail.com>
2025-01-05 16:11:36 +0700

    feat: api profile player

--- app/player/controller.js
+++ app/player/controller.js
@@ -5,6 +5,9 @@ const Bank = require("../bank/model");
 const Payment = require("../payment/model");
 const Category = require("../category/model");
 const Transaction = require("../transaction/model");
+const path = require("path");
+const fs = require("fs");
+const { rootPath } = require("../../config");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -208,6 +211,99 @@ module.exports = {
       res
         .status(500)
         .json({ message: error.message || "Internal server error" });
+    }
+  },
+  profile: async (req, res) => {
+    try {
+      const player = {
+        id: req.player._id,
+        username: req.player.username,
+        email: req.player.email,
+        name: req.player.name,
+        avatar: req.player.avatar,
+        phone_number: req.player.phoneNumber,
+      };
+
+      res.status(200).json({ data: player });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: error.message || "Internal server error" });
+    }
+  },
+  editProfile: async (req, res) => {
+    try {
+      const { name = "", phoneNumber = "" } = req.body;
+      const payload = {};
+
+      if (name.length) payload.name = name;
+      if (phoneNumber.length) payload.phoneNumber = phoneNumber;
+
+      if (req.file) {
+        let tmp_path = req.file.path;
+        let originalExt =
+          req.file.originalname.split(".")[
+            req.file.originalname.split(".").length - 1
+          ];
+        let filename = req.file.filename + "." + originalExt;
+        let target_path = path.resolve(rootPath, `public/uploads/${filename}`);
+
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
+
+        src.pipe(dest);
+        src.on("end", async () => {
+          let player = await Player.findOne({ _id: req.player._id });
+
+          let currentImage = `${rootPath}/public/uploads/${player.avatar}`;
+          if (fs.existsSync(currentImage)) {
+            fs.unlinkSync(currentImage);
+          }
+
+          player = await Player.findOneAndUpdate(
+            {
+              _id: req.player._id,
+            },
+            {
+              ...payload,
+              avatar: filename,
+            },
+            { new: true, runValidators: true }
+          );
+
+          res.status(201).json({
+            data: {
+              _id: player.id,
+              name: player.name,
+              phoneNumber: player.phoneNumber,
+              avatar: player.avatar,
+            },
+          });
+        });
+      } else {
+        const player = await Player.findOneAndUpdate(
+          {
+            _id: req.player._id,
+          },
+          payload,
+          { new: true, runValidators: true }
+        );
+
+        res.status(200).json({
+          _id: player.id,
+          name: player.name,
+          phoneNumber: player.phoneNumber,
+          avatar: player.avatar,
+        });
+      }
+    } catch (error) {
+      if (error && error.name === "ValidationError") {
+        res.status(422).json({
+          error: 1,
+          message: error.message,
+          fields: error.errors,
+        });
+      }
     }
   },
 };
--- app/player/router.js
+++ app/player/router.js
@@ -8,8 +8,12 @@ const {
   history,
   historyDetail,
   dashboard,
+  profile,
+  editProfile,
 } = require("./controller");
 const { isLoginPlayer } = require("../middleware/auth");
+const multer = require("multer");
+const os = require("os");
 
 router.get("/landingpage", landingPage);
 router.get("/:id/detail", detailPage);
@@ -18,5 +22,12 @@ router.post("/checkout", isLoginPlayer, checkout);
 router.get("/history", isLoginPlayer, history);
 router.get("/history/:id/detail", isLoginPlayer, historyDetail);
 router.get("/dashboard", isLoginPlayer, dashboard);
+router.get("/profile", isLoginPlayer, profile);
+router.put(
+  "/profile",
+  isLoginPlayer,
+  multer({ dest: os.tmpdir() }).single("image"),
+  editProfile
+);
 
 module.exports = router;
